b064f995b465e7955cee0c4da3b435c77e1e22f2
RajshriSkillup <132663983+RajshriSkillup@users.noreply.github.com>
2025-09-10 11:57:24 +0530

    Update factories.py

--- tests/factories.py
+++ tests/factories.py
@@ -46,4 +46,16 @@ class ProductFactory(factory.Factory):
             "Wrench",
         ]
     )
-  
+    description = factory.Faker("text")
+    price = FuzzyDecimal(0.5, 2000.0, 2)
+    available = FuzzyChoice(choices=[True, False])
+    category = FuzzyChoice(
+        choices=[
+            Category.UNKNOWN,
+            Category.CLOTHS,
+            Category.FOOD,
+            Category.HOUSEWARES,
+            Category.AUTOMOTIVE,
+            Category.TOOLS,
+        ]
+    )
