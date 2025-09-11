7df0ca7532d304a9553a0f07b684acd04e0481d4
RajshriSkillup <132663983+RajshriSkillup@users.noreply.github.com>
2025-09-11 16:10:19 +0530

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -110,16 +110,6 @@ def list_products():
     if name:
         app.logger.info("Find by name: %s", name)
         products = Product.find_by_name(name)
-    elif category:
-        app.logger.info("Find by category: %s", category)
-        # create enum from string
-        category_value = getattr(Category, category.upper())
-        products = Product.find_by_category(category_value)
-    else:
-        app.logger.info("Find by available: %s", available)
-        # create bool from string
-        available_value = available.lower() in ["true", "yes", "1"]
-        products = Product.find_by_availability(available_value)
 
     results = [product.serialize() for product in products]
     app.logger.info("[%s] Products returned", len(results))
